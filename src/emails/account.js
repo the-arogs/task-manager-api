41f9c7a263292c8c87d114f2cf43665304aa116a
Arogs <user@MACs-MacBook-Pro.local>
2022-03-09 11:37:54 +0100

    update security

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,10 +1,10 @@
 const sgMail = require('@sendgrid/mail')
-const from = 'tomilsonargos@gmail.com'
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
  const sendWelcEmail = (email, name)=> {sgMail.send({
     to: email,
-    from ,
+    
     subject: 'This is a test',
     text: `Welcome to the app, ${name}. Enjoy `
 })}
@@ -12,7 +12,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendExitMail = (email, name) => {
     sgMail.send({
         to: email,
-        from,
+        
         subject: 'Goodbye',
         text: `Just before you go ${name}, please tell us more`
 
